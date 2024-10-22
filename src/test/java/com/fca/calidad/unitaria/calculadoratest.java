43cf746aa24328ab5c44cbec45ecdd4d37f64fbc
fca <fca@FCA-Contpaqi-17.inet.uady.mx>
2024-10-21 19:30:59 -0600

    cambios codigo

--- src/test/java/com/fca/calidad/unitaria/calculadoratest.java
+++ src/test/java/com/fca/calidad/unitaria/calculadoratest.java
@@ -1,14 +1,22 @@
 package com.fca.calidad.unitaria;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class calculadoratest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void suma2numerosPositivostest()
+	{
+		double num1=2;
+		double num2 =5;
+		double resEsperado=7;
+		calculadora calculadora =new calculadora();
+		//Ejercicio, llamar al metodo que queremos probar 
+		double resEjecucion =calculadora.suma(num1, num2);
+		
+		//verificar
+	//	asserThat(resEsperado, is(resEjecucion));
 	}
-
 }
