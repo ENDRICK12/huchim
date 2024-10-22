16068fb0a04a020f584f2d797696ab30c76306b6
fca <fca@FCA-Contpaqi-17.inet.uady.mx>
2024-10-21 19:28:49 -0600

    codigo de incio

---
+++ src/test/java/com/fca/calidad/unitaria/calculadoratest.java
@@ -0,0 +1,14 @@
+package com.fca.calidad.unitaria;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class calculadoratest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
