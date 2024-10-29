d85e50055a5262f1f7c62b21085f172e681e1c3e
fca <fca@FCA-Contpaqi-17.inet.uady.mx>
2024-10-28 19:01:03 -0600

    finalisa el mockito

--- src/test/java/com/fca/calidad/doubles/Dependencytest.java
+++ src/test/java/com/fca/calidad/doubles/Dependencytest.java
@@ -1,13 +1,22 @@
 package com.fca.calidad.doubles;
 
+
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
 
 import com.fca.calidad.doubles.Dependency.SubDependency;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.mockito.stubbing.Answer;
 
 class Dependencytest {
 private Dependency dependency;
@@ -21,7 +30,37 @@ void setup() {
 	
 }
 
-	@Test
+@Test 
+void addTwoTest() {
+
+	//inicializacion
+	
+	when(dependency.addTwo(2)).thenReturn(10);
+	int resultadoEsperado =12;
+
+	
+//assertThat(resultadoEsperado, is(dependency.addTwo(5)));
+	
+	
+}
+
+@Test 
+void addTwoAnswerTest() {
+	
+	when(dependency.addTwo(anyInt())).thenAnswer(new Answer<Integer>() {
+		public Integer answer(InvocationOnMock invocation) throws Throwable{
+			int arg=(Integer)invocation.getArguments()[0];
+			return 20* 2+10+arg;
+		}
+	});
+	
+	
+	
+}
+
+
+
+	@Test	
 	void test() {
 		System.out.println(sub.getClassName());
 	}
@@ -30,9 +69,12 @@ void setup() {
 	@Test
 
 	
+	
+	
 	public void testsubDependency () {
 		//inicializacion 
 		when (sub.getClassName()).thenReturn("hola");
+		String resultadoEsperado="hola";
 		//ejercicio
 		String result =sub.getClassName();
 		//verificacion
