17a34e2c87be56add960c84aaa6a4f8a27de3aeb
fca <fca@FCA-Contpaqi-17.inet.uady.mx>
2024-11-20 19:24:26 -0600

    nuevas pruebas

--- src/main/java/com/fca/calidad/maven/App.java
+++ src/main/java/com/fca/calidad/maven/App.java
@@ -1,5 +1,8 @@
 package com.fca.calidad.maven;
 
+import com.fca.calidad.dao.DAOUserSQLite;
+import com.fca.calidad.model.User.User;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        DAOUserSQLite dao = new DAOUserSQLite();
+        User usuario = new User ("nombre" , "correo","password");
+        int id = dao.save (usuario);
+        
+        usuario.setId(id);
+        dao.findById(id);
+        
+        System.out.println(dao.findById(id).toString());
+        
+        dao.deleteById(2);
+        
     }
 }
